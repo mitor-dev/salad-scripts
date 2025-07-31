800f6ba74076109a7d01155d99f45a6fb56d1bec
mitor-dev <mitor.dev@gmail.com>
2025-07-31 12:01:58 +0530

    comfyui_setup.sh

--- comfyui_setup.sh
+++ comfyui_setup.sh
@@ -47,15 +47,6 @@ download_model() {
 }
 
 # === DOWNLOAD MODELS ===
-download_model "v1-5-pruned-emaonly_v15PrunedEmaonly" "https://civitai.com/api/download/models/66991?type=Model&format=SafeTensor&size=full&fp=fp16"
 download_model "RealisticVisionV60B1_v51HyperVAE" "https://civitai.com/api/download/models/501240?type=Model&format=SafeTensor&size=full&fp=fp16"
-download_model "SDXL_v10VAEFix" "https://civitai.com/api/download/models/128078?type=Model&format=SafeTensor&size=pruned&fp=fp16"
-download_model "epiCRealism_naturalSinRC1VAE" "https://civitai.com/api/download/models/143906?type=Model&format=SafeTensor&size=pruned&fp=fp16"
-download_model "DreamShaper_8" "https://civitai.com/api/download/models/128713?type=Model&format=SafeTensor&size=pruned&fp=fp16"
-download_model "JuggernautXL_ragnarokBy" "https://civitai.com/api/download/models/1759168?type=Model&format=SafeTensor&size=full&fp=fp16"
-download_model "AbsoluteReality_v181" "https://civitai.com/api/download/models/132760?type=Model&format=SafeTensor&size=pruned&fp=fp16"
-download_model "HassakuXLIllustrious_v30" "https://civitai.com/api/download/models/2010753?type=Model&format=SafeTensor&size=pruned&fp=fp16"
-download_model "CounterfeitXL_v25" "https://civitai.com/api/download/models/265012?type=Model&format=SafeTensor&size=pruned&fp=fp16"
-download_model "MeinaMix_meinaV11" "https://civitai.com/api/download/models/119057?type=Model&format=SafeTensor&size=pruned&fp=fp16"
 
 echo "✅ comfyui_setup.sh complete."
